d858a0eb3d64404b13bbff1347a056682f67a805
HarithKavish <harithkavish40@gmail.com>
2023-02-21 12:50:23 +0530

    commiteg1

---
+++ src/main/java/com/example/demo/Eg1HelloApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eg1HelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eg1HelloApplication.class, args);
+	}
+
+}
